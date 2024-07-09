6b6e39dd9317fadaea992af06900f8a653c5d1d6
Shiying Wang <you@example.com>
2024-07-09 16:53:24 -0700

    attemp 2

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# Chap7_Lab-6: Create the reverse order list with pop()?
+# Shiying Wang
+# COMSC 140
+# 9-Jul-2024
 
 def getInput():
     values=input('Enter values (separate them with spaces: ')
@@ -6,16 +10,20 @@ def getInput():
 
 
 def makeReverse(numbers):
+    reverse = []
     for i in range(len(numbers)):
-        numbers.pop(i)
-        reverse = []
-        reverse = numbers.insert(i, numbers[len(numbers)-i-1])
+        reverse.insert(i, numbers[len(numbers)-1-i])
+    # for i in range(len(numbers)):
+    #     numbers.pop(i)
+    #     reverse = []
+    #     reverse = numbers.insert(i, numbers[len(numbers)-i-1])
     return reverse
 
 def main():
     numbers = getInput()
     ret = makeReverse(numbers)
-    print(f'The result values are: {ret}')
+    print(f'The result values are: {ret}') 
+    #  The result values are: [2, 8, 2, 5, 1]
 
 
 if __name__ == '__main__':
